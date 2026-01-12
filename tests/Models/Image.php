519b7d5dee304534213a32c7084c644639842561
gigerit <info@gigerit.ch>
2026-01-12 15:46:51 +0100

    feat: add property annotations to Image model for better IDE support
    - Added PHPDoc property annotations to the Image model to improve code readability and provide better autocompletion in IDEs.
    - Included properties for `id`, `url`, `imageable_type`, and `imageable_id` to enhance type hinting and documentation.

--- tests/Models/Image.php
+++ tests/Models/Image.php
@@ -4,6 +4,12 @@ namespace Gigerit\LaravelCascadeDelete\Tests\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $url
+ * @property string $imageable_type
+ * @property int $imageable_id
+ */
 class Image extends Model
 {
     protected $guarded = [];
